88a308e3f6b848380b619a08b5822465240b49eb
Anish <anish.vilayil.s@gmail.com>
2019-03-10 08:06:53 +0530

    second commit

---
+++ arduino/Final_SmartBin/Final_SmartBin.ino
@@ -0,0 +1,219 @@
+
+
+#include<SoftwareSerial.h>
+
+SoftwareSerial client(2,3); // pin Connection : RX to 3, TX to 2
+
+
+const int trigPin = 8;
+
+const int echoPin = 9;
+String webpage="";
+int i=0,k=0;
+String readString;
+int x=0;
+
+boolean No_IP=false;
+String IP="";
+char temp1='0';
+
+String name="<p>Circuit Digest</p>";   //22
+String dat="<p>Data Received Successfully.....</p>";     //21
+     
+void check4IP(int t1)
+{
+  int t2=millis();
+  while(t2+t1>millis())
+  {
+    while(client.available()>0)
+    {
+      if(client.find("WIFI GOT IP"))
+      {
+        No_IP=true;
+      }
+    }
+  }
+}
+
+void get_ip()
+{
+   Serial.print(" Fetching IP Address:");
+  IP="";
+  char ch=0;
+  while(1)
+  {
+    client.println("AT+CIFSR");
+    while(client.available()>0)
+    {
+      if(client.find("STAIP,"))
+      {
+        delay(1000);
+        Serial.print("IP Address:");
+        while(client.available()>0)
+        {
+          ch=client.read();
+          if(ch=='+')
+          break;
+          IP+=ch;
+        }
+      }
+      if(ch=='+')
+      break;
+    }
+    if(ch=='+')
+    break;
+    delay(1000);
+  }
+  Serial.print(IP);
+  Serial.print("Port:");
+  Serial.println(80);
+}
+
+void connect_wifi(String cmd, int t)
+{
+  int temp=0,i=0;
+  while(1)
+  {
+    Serial.println(cmd);
+    client.println(cmd); 
+    while(client.available())
+    {
+      if(client.find("OK"))
+      i=8;
+    }
+    delay(t);
+    if(i>5)
+    break;
+    i++;
+  }
+  if(i==8)
+  Serial.println("OK");
+  else
+  Serial.println("Error");
+}
+
+void wifi_init()
+{
+      connect_wifi("AT",100);
+      connect_wifi("AT+CWMODE=3",100);
+      connect_wifi("AT+CWQAP",100);  
+      connect_wifi("AT+RST",5000);
+      check4IP(5000);
+      if(!No_IP)
+      {
+        Serial.println("Connecting Wifi....");
+        connect_wifi("AT+CWJAP=\"Vilayil Puthen Veedu\",\"Vilayil@123\"",7000);         //provide your WiFi username and password here
+     // connect_wifi("AT+CWJAP=\"vpn address\",\"wireless network\"",7000);
+      }
+      else
+        {
+        }
+      Serial.println("Wifi Connected"); 
+      //get_ip();
+      connect_wifi("AT+CIPMUX=1",100);
+      connect_wifi("AT+CIPSERVER=1,80",100);
+}
+
+void sendwebdata(String webPage)
+{
+    int ii=0;
+     while(1)
+     {
+      unsigned int l=webPage.length();
+      Serial.print("AT+CIPSEND=0,");
+      client.print("AT+CIPSEND=0,");
+      Serial.println(l+2);
+      client.println(l+2);
+      delay(100);
+      Serial.println(webPage);
+      client.println(webPage);
+      while(client.available())
+      {
+        //Serial.print(Serial.read());
+        if(client.find("OK"))
+        {
+          ii=11;
+          break;
+        }
+      }
+      if(ii==11)
+      break;
+      delay(100);
+     }
+}
+
+void setup() 
+{
+   Serial.begin(115200);
+   client.begin(115200);
+   wifi_init();
+   Serial.println("System Ready..");
+
+
+   
+}
+
+void printResponse() {
+  while (client.available()) {
+    Serial.println(client.readStringUntil('\n')); 
+  }}
+  
+
+void loop() 
+{
+
+ long duration, cm;
+
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  pinMode(trigPin, OUTPUT);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+
+
+  cm = duration / 29 / 2;
+  
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  
+  delay(100);
+ 
+    //  Send to Server;
+
+ client.println("AT+CIPMUX=1");
+  delay(1000);
+  printResponse();
+
+  client.println("AT+CIPSTART=4,\"TCP\",\"192.168.31.79\",80");
+  delay(1000);
+  printResponse();
+
+  String cmd = "GET /smartbin/web/insert.php?q="+String(cm);
+  client.println("AT+CIPSEND=4," + String(cmd.length() + 4));
+  delay(1000);
+
+  client.println(cmd);
+  delay(1000);
+  client.println();
+  delay(1000);
+  printResponse();
+
+  delay(5000);
+
+    
+delay(10000);
+}
+
+
+
+
